d51a392f37e0e1641c6dd14cac4d741522487255
DJ <mail@chenquanyi.cn>
2022-11-14 16:56:37 +0800

    初始化工程

---
+++ src/test/java/com/pspo/eureka/PspoEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pspo.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspoEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
